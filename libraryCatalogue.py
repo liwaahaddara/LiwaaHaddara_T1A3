9e4e0520bfb1497919a6745707c7aed857d1fa8c
liwaahaddara <12786@coderacademy.edu.au>
2022-07-17 16:20:50 +1000

    added neccessary import statements and two functions that print the catalogue and add books to it respectively

--- libraryCatalogue.py
+++ libraryCatalogue.py
@@ -1,6 +1,21 @@
+import imp
+from libraryBook import LibraryBook
+from os import system
 
 
 class LibraryCatalogue():
     # a list of books in the catalogue
     def __init__(self, catalogue_items):
         self.catalogue_items = catalogue_items
+
+    # prints the current catalogue
+    def print_catalogue(self):
+        system("clear")
+        print("\n\nHere is our catalogue:\n")
+        for book in self.catalogue_items:
+            book.show_book()
+
+    # this function adds a book to the catalogue
+    def add_book(self, title, author):
+        new_book = LibraryBook(title, author)
+        self.catalogue_items.append(new_book)
