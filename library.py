e40946f91423ad28f1ec709d43505604e23e4b9a
liwaahaddara <12786@coderacademy.edu.au>
2022-07-17 16:28:13 +1000

    added code for printing the main menu when the program is run (located in 'library.py')

--- library.py
+++ library.py
@@ -4,3 +4,20 @@ from librarySeed import library_seed
 
 catalogue = library_seed()
 rented_books = []
+
+
+def print_options():
+    print(f"""
+Welcome to Liwaa's Library! Please select an option
+from the following menu:
+
+1. Show Current Book Catalogue
+2. Add book to the Catalogue
+3. Delete a book
+4. Rent a book from the Catalogue
+5. Show books I'm currently renting
+6. Exit
+    """)
+    opt = input("Select your option (1-6): ")
+
+    return opt
